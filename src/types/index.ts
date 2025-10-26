3d567896c85b99de51b7f21fb7b87323b4bb1285
sara atiyabi <saraatiyabi18@gmail.com>
2025-10-26 22:18:53 +0330

    update(images): adding static images data and typescript interface

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+// src/types/index.ts
+export interface HikingImage {
+  id: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  tags: string[];
+  thumb: string;
+  full: string;
+}
